e391e2680f8e9fd24b60b5b4245f2cf45e6dc0c3
Jean-Noel-Esc <jeannoelescande@gmail.com>
2024-12-02 02:05:23 +0100

    Update CORS configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": ["https://data-jobs-board.onrender.com"],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"]
+    }
+})
 
 from app import routes
